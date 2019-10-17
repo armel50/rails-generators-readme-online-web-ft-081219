091d9a8c43545ce6ecf77cc1857135e5f03818c5
Cloe Kouadjo <armelfli50@gmail.com>
2019-10-17 02:04:26 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191017015248) do
+ActiveRecord::Schema.define(version: 20191017020253) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20191017015248) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.string   "post_status"
   end
 
 end
