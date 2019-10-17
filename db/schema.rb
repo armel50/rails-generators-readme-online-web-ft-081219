b0b8f4a8ab34e299451c945115252ef2d3b499ec
Cloe Kouadjo <armelfli50@gmail.com>
2019-10-17 02:08:45 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191017020253) do
+ActiveRecord::Schema.define(version: 20191017020755) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.string "genre"
+    t.text   "bio"
   end
 
   create_table "categories", force: :cascade do |t|
